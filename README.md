a99e542c74ec8e72807672a03a5762dfcb1994b9
v.mayer <vincent.mayer@agile-robots.com>
2022-03-25 08:57:56 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Hack Assembler in C++
+
+This is a simple C++ implementation of an assembler for the Hack machine language.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    if (argc == 1) {
+        std::cout << "You must provide "
+    }
+    int i = 0;
+    return 0;
+}
