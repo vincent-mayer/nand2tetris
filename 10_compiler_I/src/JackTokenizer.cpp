589a2240acedb52f2c75fb57013361a63f942e01
v.mayer <vincent.mayer@agile-robots.com>
2022-09-03 16:12:19 +0200

    finally fucking finish the jacktokenizer ( the worst code I've written lol)

--- 10_compiler_I/src/JackTokenizer.cpp
+++ 10_compiler_I/src/JackTokenizer.cpp
@@ -15,23 +15,32 @@ auto JackTokenizer::advance() -> void
         if (m_line_it == m_line.end())
         {
             getline(m_file, m_line);
-            m_line_it = m_line.begin();
 
-            // Remove comment section from command and the weird end-line char
+            // Remove comment section from command, by just ignoring the entire line.
             if (m_line.find("//") != std::string::npos ||
                 m_line.find("/*") != std::string::npos ||
-                m_line.find("*/") != std::string::npos ||
-                m_line.find("\r") != std::string::npos)
+                m_line.find("*/") != std::string::npos)
             {
                 m_line = m_line.substr(0, m_line.find("//"));
                 m_line = m_line.substr(0, m_line.find("/*"));
                 m_line = m_line.substr(0, m_line.find("//"));
-                m_line = m_line.substr(0, m_line.find("\r"));
-                m_line_it = m_line.begin();
             }
 
+            // Remove the \r token.
+            if (m_line.find("\r") != std::string::npos)
+            {
+                m_line.erase(m_line.find("\r"), std::string{"\r"}.length());
+            }
+            if (m_line.find("\t") != std::string::npos)
+            {
+                m_line.erase(m_line.find("\t"), std::string{"\t"}.length());
+            }
+
+            // Set the line it to the beginning.
+            m_line_it = m_line.begin();
+
             // Skip empty lines and pure comments
-            if ((m_line.size() <= 1))
+            if ((m_line.size() < 1))
             {
                 m_line_it = m_line.end();
                 continue;
@@ -52,25 +61,74 @@ auto JackTokenizer::advance() -> void
         // If there is still whitespace left, read characters until next space
         std::string cur_substr{m_line_it, m_line.end()};
         auto token_end_pos = cur_substr.find(" ");
+
         // If we have a leading white space just increase the iterator and continue
         if (token_end_pos == 0)
         {
             m_line_it++;
             continue;
         }
+
+        // Handle the case for when the current subtring contains a string constant
+        auto quote_pos = cur_substr.find("\"");
+        if (quote_pos < token_end_pos)
+        {
+            // If quote before space.
+            token_end_pos = quote_pos - 1;
+        }
+        if (quote_pos == 0)
+        {
+            // Find the second quote.
+            auto string_end_pos = cur_substr.rfind("\"");
+
+            // Return token
+            m_token = std::string(m_line_it, m_line_it + string_end_pos + 1);
+            is_token = true;
+            m_line_it += string_end_pos + 1;
+            std::cout << m_token << std::endl;
+            break;
+        }
+
         if (token_end_pos != std::string::npos)
         {
             // Remove any 'single char tokens' from the end and any whitespace in between
-            while (
-                (std::find(tokens.begin(), tokens.end(),
-                           std::string{*(m_line_it + token_end_pos)}) != tokens.end() ||
-                 std::string{*(m_line_it + token_end_pos)} == " ") &&
-                token_end_pos > 1)
+            while (true)
             {
-                token_end_pos--;
+                auto end_char = std::string{*(m_line_it + token_end_pos)};
+
+                // Sepecial handling for string constants, i.e. quotation marks.
+                if (end_char == "\"")
+                {
+                    auto quote_pos = cur_substr.find(end_char);
+                    token_end_pos = token_end_pos - (cur_substr.length() - quote_pos);
+                }
+
+                auto known_token_pos = std::find(tokens.begin(), tokens.end(), end_char);
+                auto is_known_token = known_token_pos != tokens.end();
+                auto is_space = end_char == " ";
+                auto is_not_empty = token_end_pos >= 1;
+
+                // Then reduce the token end_pos
+                if ((is_known_token || is_space) && is_not_empty)
+                    token_end_pos--;
+                else
+                    break;
             }
 
             // Get the token
+            auto maybe_token = std::string(m_line_it, m_line_it + token_end_pos + 1);
+
+            // Handle case for when there is still a special character in between
+            for (auto &c : maybe_token)
+            {
+                auto special_char =
+                    std::find(tokens.begin(), tokens.end(), std::string{c});
+                if (special_char != tokens.end())
+                {
+                    token_end_pos = maybe_token.find(std::string{*special_char}) - 1;
+                    break;
+                }
+            }
             m_token = std::string(m_line_it, m_line_it + token_end_pos + 1);
             is_token = true;
             m_line_it += token_end_pos + 1;
@@ -88,6 +146,20 @@ auto JackTokenizer::advance() -> void
                 token_length--;
                 token_end_it--;
             }
+            // Now there might still be the case that there are special fucking symbols
+            // hidden somwhere. So find them and eleminate them!!!!!!!
+            auto maybe_token = std::string(m_line_it, m_line_it + token_length);
+            for (auto &c : maybe_token)
+            {
+                auto special_char =
+                    std::find(tokens.begin(), tokens.end(), std::string{c});
+                if (special_char != tokens.end())
+                {
+                    token_length = maybe_token.find(std::string{*special_char});
+                    break;
+                }
+            }
+
             m_token = std::string(m_line_it, m_line_it + token_length);
             is_token = true;
             m_line_it += token_length;
